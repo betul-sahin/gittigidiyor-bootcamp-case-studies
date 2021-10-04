589a3688e794b9b54e0cfb6227900d73686f9d22
Betül Şahin <betulsahin3387@hotmail.com>
2021-10-04 10:06:07 +0300

    Changes

--- fourth-homework-betul-sahin/SchoolManagementSystemDemoV4/src/main/java/com/betulsahin/schoolmanagementsystemdemov4/exceptions/StudentIsAlreadyRegisteredThisCourseExistException.java
+++
@@ -1,7 +0,0 @@
-package com.betulsahin.schoolmanagementsystemdemov4.exceptions;
-
-public class StudentIsAlreadyRegisteredThisCourseExistException extends RuntimeException{
-    public StudentIsAlreadyRegisteredThisCourseExistException(String message){
-        super(message);
-    }
-}
--- fourth-homework-betul-sahin/SchoolManagementSystemDemoV4/src/main/java/com/betulsahin/schoolmanagementsystemdemov4/exceptions/StudentNumberForOneCourseExceededException.java
+++
@@ -1,7 +0,0 @@
-package com.betulsahin.schoolmanagementsystemdemov4.exceptions;
-
-public class StudentNumberForOneCourseExceededException extends RuntimeException{
-    public StudentNumberForOneCourseExceededException(String message){
-        super(message);
-    }
-}
